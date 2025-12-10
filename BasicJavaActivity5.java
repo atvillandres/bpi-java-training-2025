acfd3631b4571793f65d43547a2ca2d356b23b8c
Armando T. Villandres <atvillandres@headoffice.corp.bpi.com.ph>
2025-12-10 15:08:50 +0800

    Module 1 - Introduction to Java (Activity 5)

---
+++ BasicJavaActivity5.java
@@ -0,0 +1,22 @@
+package M1_Activity5;
+
+/**
+ * 
+ */
+
+public class BasicJavaActivity5 {
+    public static void main(String[] args) {
+   		
+    	int i = 1;
+    	int sum = 0;
+		
+        while (i <= 50) {
+            sum = sum  + i;
+            i++; // Increment variable i
+
+        }
+        
+		System.out.println("Sum = " + sum);
+        
+    }
+}
